33ebadefedebae1c28071884af23998a724ffc0b
Javier Riera Chirivella <mrvizious@gmail.com>
2019-01-15 18:01:34 +0100

    First tests with movement
    Pretty shitty right now, tbh. Added a couple sprites and things to try out the movement

---
+++ Assets/Scripts/StickmanController.cs
@@ -0,0 +1,47 @@
+﻿/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickmanController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class StickmanController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+    public float maxSpeed;
+    // Start is called before the first frame update
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        Debug.Log("Input in Horizontal axis is: " + Input.GetAxis("Horizontal"));
+    }
+
+    void FixedUpdate(){
+        rb.AddForce(Vector2.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime);
+        rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
+    }
+}
