b6f57cd5fd729c8f6cd7650049df39580ef57992
Javier <mrvizious@gmail.com>
2021-01-09 19:07:39 +0100

    AI Input Recorder began

---
+++ Artificial Stupidity/Assets/Scripts/Input/AIInputRecorder.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIInputRecorder : InputManager
+{
+    public enum AIAction
+    {
+        Jump,
+        RunForward,
+        RunBackward,
+        StopRunning
+    }
+    public bool debug, recording;
+    public KeyCode recordKey;
+    public string recordingName;
+
+
+    private List<KeyValuePair<AIAction, float>> actions;
+    private float lastHorizontalInput = 0;
+    private float currentRecordingTime = 0f;
+
+    void Start()
+    {
+        actions = new List<KeyValuePair<AIAction, float>>();
+    }
+
+    void Update()
+    {
+        if (recording)
+        {
+            currentRecordingTime += Time.deltaTime;
+            if (UpdateJumpButtonDown())
+            {
+                RecordAction(AIAction.Jump, currentRecordingTime);
+            }
+            else if (UpdateHorizontalInput() != lastHorizontalInput)
+            {
+                if (horizontalInput > 0) RecordAction(AIAction.RunForward, currentRecordingTime);
+                else if (horizontalInput < 0) RecordAction(AIAction.RunBackward, currentRecordingTime);
+                else RecordAction(AIAction.StopRunning, currentRecordingTime);
+            }
+        }
+    }
+
+    public void ToggleRecording()
+    {
+        if (!recording)
+        {
+            if (debug) Debug.Log("Started recording called: " + recordingName);
+            currentRecordingTime = 0f;
+            recording = true;
+        }
+        else
+        {
+            if (debug) Debug.Log("STOPPED recording called: " + recordingName);
+            WriteRecordingToCSV();
+            recording = false;
+        }
+    }
+
+    private void WriteRecordingToCSV()
+    {
+        if (debug) Debug.Log("Writing recording with name " + recordingName + " to CSV");
+        //TODO: Record to CSV
+    }
+
+    private void RecordAction(AIAction newAction, float time)
+    {
+        if (debug) Debug.Log(newAction.ToString() + " recorded at: " + time + " seconds");
+        actions.Add(
+            new KeyValuePair<AIAction, float>
+            (newAction, time)
+        );
+    }
+}
---
+++ Artificial Stupidity/Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class InputManager : MonoBehaviour
+{
+    public Dictionary<KeyCode, bool> keyCodesDown;
+    public bool jumpButtonDown = false;
+    public int horizontalInput = 0;
+
+    public void AddKeyCode(KeyCode newKey)
+    {
+        if (!keyCodesDown.ContainsKey(newKey))
+        {
+            keyCodesDown.Add(newKey, false);
+        }
+    }
+
+    protected bool UpdateJumpButtonDown()
+    {
+        jumpButtonDown = Input.GetButtonDown("Jump");
+        return jumpButtonDown;
+    }
+
+    protected int UpdateHorizontalInput()
+    {
+        horizontalInput = (int)Input.GetAxisRaw("Horizontal");
+        return horizontalInput;
+    }
+
+    protected void UpdateKeyCodesDown()
+    {
+        foreach (KeyValuePair<KeyCode, bool> pair in keyCodesDown)
+        {
+            keyCodesDown[pair.Key] = Input.GetKeyDown(pair.Key);
+        }
+    }
+}
