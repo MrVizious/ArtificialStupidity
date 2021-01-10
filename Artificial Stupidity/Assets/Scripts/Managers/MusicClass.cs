527973804f9441858c4af9a2e3b809dd5aa6ee2f
Javier <mrvizious@gmail.com>
2021-01-10 22:48:21 +0100

    Music downloaded

---
+++ Artificial Stupidity/Assets/Scripts/Managers/MusicClass.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicClass : MonoBehaviour
+{
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        GetComponent<AudioSource>().Play();
+    }
+}
