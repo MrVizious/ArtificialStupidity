1c908db21107160c5249c7f5e07b3e4b5fbfe20b
Javier <mrvizious@gmail.com>
2021-01-10 06:29:30 +0100

    AI animation controller created

--- Artificial Stupidity/Assets/Scripts/Platforms/Activation Method/ActivationMethod.cs
+++ Artificial Stupidity/Assets/Scripts/Platforms/Activation Method/ActivationMethod.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Platform))]
 public abstract class ActivationMethod : MonoBehaviour
 {
     protected Platform platform;
